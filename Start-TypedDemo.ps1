e8d9255c5fd58b584b97b28bba2f22832a2f6b4b
Mötz Jensen <motz@e-s.dk>
2019-03-22 14:14:28 +0100

    Update Start-TypedDemo.ps1

--- Start-TypedDemo.ps1
+++ Start-TypedDemo.ps1
@@ -417,8 +417,6 @@ Copyright (C) Microsoft Corporation. All rights reserved.
 
                     if($char -eq '`') {
                         $color = $previousColor
-                    }elseif($char -eq '|') {
-                        $color = $colorCommandName
                     }
                 }
 
